6853922d72ca6e94d3c8bd18f5b3d69f04fd623c
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-22 12:56:20 -0400

    agregando else al momento de verificar si se esta enviando una url

--- content/controllers/frontController.php
+++ content/controllers/frontController.php
@@ -1,11 +1,21 @@
 <?php
+
 namespace content\controllers;
 
 use config\settings\sysConfig as sysConfig;
 
 
-class frontController extends sysConfig{
-  
-}
+class frontController extends sysConfig
+{
+  private $url;
+  public function __construct($request)
+  {
 
-?>
+    if (isset($request)) {
+      $this->url = $request['url'];
+    }else{
+      die("<script>location='?url=home'</script>");
+    }
+    
+  }
+}
