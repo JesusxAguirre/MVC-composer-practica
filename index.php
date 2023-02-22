b9bd0227b3887a817cf68413758da2f8fae01ec6
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-22 10:43:59 -0400

    verificando que exista el autoload

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
+if (file_exists("vendor/autoload/php")) {
 
-require 'vendor/autoload.php';
+  require_once("vendor/autoload.php");
+} else {
+  if (file_exists("content/component/error.php")) {
+
+    require_once("content/component/error.php");
+  }else{
+    echo "ERROR COMUNIQUISE CON EL EQUIPO DE MANTENIMIENTO";
+  }
+}
 
 use ElectivaMvcPractica\Content\clase_test as clase_test;
 
 new clase_test();
-
-?>
