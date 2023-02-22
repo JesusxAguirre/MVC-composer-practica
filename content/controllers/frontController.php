d3d622cca8002d5cf30b4e3ddae6244021955247
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-22 13:39:39 -0400

    obteniendo ruta de archivos en controller

--- content/controllers/frontController.php
+++ content/controllers/frontController.php
@@ -8,14 +8,33 @@ use config\settings\sysConfig as sysConfig;
 class frontController extends sysConfig
 {
   private $url;
+  private $directory;
+  private $controller;
   public function __construct($request)
   {
 
     if (isset($request)) {
       $this->url = $request['url'];
-    }else{
+
+      //creando objeto sysconfig para extraer directorio de controller de constantes definidas
+      $objeto_sys = new sysConfig();
+      $this->directory = $objeto_sys->get_directory_();
+      $this->controller = $objeto_sys->get_controller_();
+      $this->validar_url();
+    } else {
       die("<script>location='?url=home'</script>");
     }
-    
+  }
+
+
+  public function validar_url()
+  {
+    $respuesta = preg_match_all("/^[a-zA-Z0-9-@\/.=:_#$ ]{1,700}$/", $this->url);
+
+    if ($respuesta == 1) {
+      //do someting
+    } else {
+      die("<script>location='?url=error_url'</script>");
+    }
   }
 }
