51b2ecf659ea3cf92ec5e195fd695e491e8cfa42
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-23 12:31:08 -0400

    creando vista de editar perfil

---
+++ view/edit-perfil_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/login.css">
+  <link rel="stylesheet" href="vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
+
+  <script src="static/js/jquery-3.6.0.min.js"></script>
+
+</head>
+
+<body class="h-100" style="background-color: #eee;">
+  <div class="container-fluid ">
+  </div>
+</body>
+
+</html>
