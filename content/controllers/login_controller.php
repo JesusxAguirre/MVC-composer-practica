3904fba41f10974caa1112dd365d3937c8fde90e
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-03-20 12:00:19 -0400

    creando autentificacion de usuario

--- content/controllers/login_controller.php
+++ content/controllers/login_controller.php
@@ -1,4 +1,25 @@
-<?php 
-require_once("view/login_view.php");
+<?php
+session_start();
 
-?>
+use ElectivaMvcPractica\Content\config\settings\sysConfig as sysConfig;
+
+
+
+if (!isset($_SESSION['token'])) {
+  $global_config = new sysConfig();
+  $_SESSION['token'] = $global_config->get_token_();
+}
+
+if (isset($_POST['token'])) {
+
+  if ($_SESSION['usuario'] == $_POST['usuario'] && password_verify($_POST['clave'], $_SESSION['clave'])) {
+    echo json_encode(array("response" => 1));
+    return true;
+  } else {
+    echo json_encode(array("response" => 0));
+    return false;
+  }
+}
+
+
+require_once("view/login_view.php");
--- content/controllers/registro_controller.php
+++ content/controllers/registro_controller.php
@@ -1,27 +1,28 @@
-<?php 
+<?php
 session_start();
+
 use ElectivaMvcPractica\Content\config\settings\sysConfig as sysConfig;
 
-$global_config = new sysConfig();
 
-if(!isset($_SESSION['token'])){
-$_SESSION['token'] = $global_config->get_token_();
+
+if (!isset($_SESSION['token'])) {
+  $global_config = new sysConfig();
+  $_SESSION['token'] = $global_config->get_token_();
 }
 
-if(isset($_POST['token'])){
-  if(hash_equals($_SESSION['token'],$_POST['token'])){
-    $_SESSION['usuario']=  $_POST['usuario'];
-    $_SESSION['clave'] = password_hash($_POST['clave'],PASSWORD_DEFAULT);
-  
-    echo json_encode(array("response"=>1));
+if (isset($_POST['token'])) {
+  if (hash_equals($_SESSION['token'], $_POST['token'])) {
+    $_SESSION['usuario'] =  $_POST['usuario'];
+    $_SESSION['clave'] = password_hash($_POST['clave'], PASSWORD_DEFAULT);
+
+    echo json_encode(array("response" => 1));
     return true;
-  }else{
-    
-    echo json_encode(array("response"=>0,"token"=>$_SESSION['token']));
-  
+  } else {
+
+    echo json_encode(array("response" => 0, "token" => $_SESSION['token']));
+
     return false;
   }
 }
 
 require_once("view/registro_view.php");
-?>
